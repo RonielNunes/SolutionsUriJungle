fd180a574281592cb45d9c2121b0c87e6a7eb501
Roniel Nunes <roniel.barbosa@mmtools.com.br>
2023-10-16 09:32:01 -0300

    fazer correção

---
+++ SolutionsInC++/3301 - sobrinho do meio.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <list>
+#include <iomanip>   // std::setprecision(2)
+//g++ 1048-AumentoDeSalario.cpp -o exec
+
+using namespace std;
+
+void solver(){
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if(b == 5){
+        cout <<"luisinho"<< endl;
+    }else if(b == 6){
+        cout <<"zezinho"<< endl;
+    }else if(b == 7){
+        cout <<"huguinho"<< endl;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    solver();
+    return 0;
+}
