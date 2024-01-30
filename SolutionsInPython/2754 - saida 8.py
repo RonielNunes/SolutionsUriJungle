bbd373b6ed08db8938c6023db2e2b02e9694aca3
Roniel Nunes <roniel.barbosa@ufv.br>
2024-01-30 20:08:32 -0300

    solver

---
+++ SolutionsInPython/2754 - saida 8.py
@@ -0,0 +1,16 @@
+def solver():
+    first = float(234.345)
+    second = float(45.698)
+
+    print(f'{first:.6f} - {second:.6f}')
+    print(f'{first:.0f} - {second:.0f}')
+    print(f'{first:.1f} - {second:.1f}')
+    print(f'{first:.2f} - {second:.2f}')
+    print(f'{first:.3f} - {second:.3f}')
+    print(f'{first:.3e} - {second:.3e}')
+    print(f'{first:.3E} - {second:.3E}')
+    print(f'{first:.3f} - {second:.3f}')
+    print(f'{first:.3f} - {second:.3f}')
+
+if __name__=='__main__':
+    solver()
