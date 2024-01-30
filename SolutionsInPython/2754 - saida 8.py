85aa9cbff1dea6ddf92bdebc793d4bf0d2536ee0
Roniel Nunes <roniel.barbosa@ufv.br>
2024-01-30 20:10:53 -0300

    update

--- SolutionsInPython/2754 - saida 8.py
+++ SolutionsInPython/2754 - saida 8.py
@@ -7,8 +7,8 @@ def solver():
     print(f'{first:.1f} - {second:.1f}')
     print(f'{first:.2f} - {second:.2f}')
     print(f'{first:.3f} - {second:.3f}')
-    print(f'{first:.3e} - {second:.3e}')
-    print(f'{first:.3E} - {second:.3E}')
+    print(f'{first:.6e} - {second:.6e}')
+    print(f'{first:.6E} - {second:.6E}')
     print(f'{first:.3f} - {second:.3f}')
     print(f'{first:.3f} - {second:.3f}')
 
