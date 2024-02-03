ab335570e41c91d6856e65d64b9b60ffd7e72c1b
Roniel Nunes <roniel.barbosa@ufv.br>
2024-02-02 21:06:45 -0300

    upload

--- SolutionsInPython/1962 - HaMuitoMuitoTempoAtras.py
+++ SolutionsInPython/1962 - HaMuitoMuitoTempoAtras.py
@@ -2,10 +2,10 @@ n = int(input())
 
 for i in range(n):
     year = int(input())
-    initial_year = 2015
+    initial_year = 2014
 
     diff = initial_year - year
-    msg = "D.C" if diff >= 0 else "A.C"
-    diff = abs(diff)  # Use a função abs para obter o valor absoluto da diferença
-    
+    msg = "D.C." if diff >= 0 else "A.C."
+    diff = abs(diff)  
+    diff = diff + 1 if msg == 'D.C' else diff
     print(f"{diff} {msg}")
