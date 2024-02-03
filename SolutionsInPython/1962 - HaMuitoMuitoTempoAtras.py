992ebb82eb5ba9fe40a9d2785f5633498f1bdd2a
Roniel Nunes <roniel.barbosa@ufv.br>
2024-02-02 21:07:01 -0300

    upload

--- SolutionsInPython/1962 - HaMuitoMuitoTempoAtras.py
+++ SolutionsInPython/1962 - HaMuitoMuitoTempoAtras.py
@@ -7,5 +7,5 @@ for i in range(n):
     diff = initial_year - year
     msg = "D.C." if diff >= 0 else "A.C."
     diff = abs(diff)  
-    diff = diff + 1 if msg == 'D.C' else diff
+    diff = diff + 1 if msg == 'D.C.' else diff
     print(f"{diff} {msg}")
